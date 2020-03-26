6cd39fe8b394874499b5671b318e4a83a9badd8f
SIVANESAN-DHANIKACHALAM <61906309+Sivanesan711@users.noreply.github.com>
2020-03-26 15:51:30 +0530

    Add files via upload

--- src/model/User.java
+++ src/model/User.java
@@ -1,1 +1,36 @@
 package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class User{
+	String name;
+	String email;
+	LocalDateTime date;
+	public User(String name,String email,LocalDateTime dateTime)
+	{
+		this.name=name;
+		this.email=email;
+		this.date=dateTime;
+		
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public void setEmail(String email) {
+		this.email=email;
+	}
+	public void setDate(LocalDateTime date) {
+		this.date=date;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	LocalDateTime getDate() {
+		return this.date;
+	}
+	
+}
